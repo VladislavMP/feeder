d966b9168a6abea993584b620997294eea34fece
Roman Androsiuk <flowergunn@gmail.com>
2020-12-02 22:15:17 +0300

    Test2

--- test_project/login.Designer.cs
+++ test_project/login.Designer.cs
@@ -100,7 +100,7 @@
             this.Controls.Add(this.username_txtbx);
             this.Controls.Add(this.login_btn);
             this.Name = "Form1";
-            this.Text = "login";
+            this.Text = "a";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
